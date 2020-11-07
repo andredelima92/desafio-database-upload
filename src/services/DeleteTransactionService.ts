cdaebe069181e36cf147dd489dc97bcd21cae99f
Andre <andredelima92@gmail.com>
2020-11-07 18:56:15 -0300

    remove transaction

--- src/services/DeleteTransactionService.ts
+++ src/services/DeleteTransactionService.ts
@@ -1,8 +1,23 @@
-// import AppError from '../errors/AppError';
+import { getRepository } from 'typeorm';
+import AppError from '../errors/AppError';
+
+import Transaction from '../models/Transaction';
+
+interface Request {
+  id: string;
+}
 
 class DeleteTransactionService {
-  public async execute(): Promise<void> {
-    // TODO
+  public async execute({ id }: Request): Promise<void> {
+    const transactionRepository = getRepository(Transaction);
+
+    const transaction = await transactionRepository.findOne(id);
+
+    if (!transaction) {
+      throw new AppError('Transacion not exists.', 400);
+    }
+
+    await transactionRepository.remove(transaction);
   }
 }
 
