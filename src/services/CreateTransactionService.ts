4f2cdadea9284c127216da81eee6f128d0ea5919
Andre <andredelima92@gmail.com>
2020-11-07 17:30:04 -0300

    create transaction and category

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,53 @@
-// import AppError from '../errors/AppError';
+import { getRepository } from 'typeorm';
+import AppError from '../errors/AppError';
 
 import Transaction from '../models/Transaction';
+import Category from '../models/Category';
+
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+  titleCategory: string;
+}
 
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({
+    titleCategory,
+    title,
+    value,
+    type,
+  }: Request): Promise<Transaction> {
+    const categoryRepository = getRepository(Category);
+
+    const category = await categoryRepository.findOne({
+      where: { title: titleCategory },
+    });
+
+    let category_id = category?.id;
+
+    if (!category) {
+      const createdCategory = categoryRepository.create({
+        title: titleCategory,
+      });
+
+      await categoryRepository.save(createdCategory);
+
+      category_id = createdCategory.id;
+    }
+
+    const transactionRepisotry = getRepository(Transaction);
+
+    const transaction = transactionRepisotry.create({
+      title,
+      value,
+      type,
+      category_id,
+    });
+
+    await transactionRepisotry.save(transaction);
+
+    return transaction;
   }
 }
 
